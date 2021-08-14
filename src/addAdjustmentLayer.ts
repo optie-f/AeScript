3b0ac37b186299337365cb7b074d2216f0fc0e55
Optie <optie96@gmail.com>
2021-08-15 03:04:13 +0900

    調整レイヤー追加を追加

---
+++ src/addAdjustmentLayer.ts
@@ -0,0 +1,32 @@
+// Copyright © 2021 Optie. All rights reserved.
+
+function addAdjustmentLayer() {
+    const activeItem = app.project.activeItem
+
+    if (!(activeItem instanceof CompItem)) {
+        return
+    }
+
+    // レイヤーを選択していればその上に追加したいが、レイヤーを追加すると選択が移るため、先に取得する
+    const topOfSelected = activeItem.selectedLayers[0]
+
+    const newSolid = activeItem.layers.addSolid(
+        [1, 1, 1],
+        'Adjustment Layer',
+        activeItem.width,
+        activeItem.height,
+        activeItem.pixelAspect,
+        activeItem.duration
+    )
+    newSolid.adjustmentLayer = true
+
+    if (topOfSelected === undefined) {
+        newSolid.moveToBeginning()
+    } else {
+        newSolid.moveBefore(topOfSelected)
+        newSolid.inPoint = topOfSelected.inPoint
+        newSolid.outPoint = topOfSelected.outPoint
+    }
+}
+
+addAdjustmentLayer()
