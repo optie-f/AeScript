11a875200fdd2298347c167ede8aa9f81a027e87
Optie <optie96@gmail.com>
2021-08-15 03:04:03 +0900

    関数名と、未選択時にエラーが出るので修正

--- src/addBlackSolid.ts
+++ src/addBlackSolid.ts
@@ -1,11 +1,15 @@
 // Copyright © 2021 Optie. All rights reserved.
 
-function main() {
+function addBlackSolid() {
     const activeItem = app.project.activeItem
 
     if (!(activeItem instanceof CompItem)) {
         return
     }
+
+    // レイヤーを選択していればその上に追加したいが、レイヤーを追加すると選択が移るため、先に取得する
+    const topOfSelected = activeItem.selectedLayers[0]
+
     const newSolid = activeItem.layers.addSolid(
         [0, 0, 0],
         'Solid',
@@ -15,12 +19,11 @@ function main() {
         activeItem.duration
     )
 
-    if (activeItem.selectedLayers.length === 0) {
+    if (topOfSelected === undefined) {
         newSolid.moveToBeginning()
     } else {
-        const topOfSelected = activeItem.selectedLayers[0]
         newSolid.moveBefore(topOfSelected)
     }
 }
 
-main()
+addBlackSolid()
