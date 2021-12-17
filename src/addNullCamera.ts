c2cd8511d701553bee2df15565219a4c38e595bc
Optie <optie96@gmail.com>
2021-12-18 00:54:20 +0900

    バグ修正

--- src/addNullCamera.ts
+++ src/addNullCamera.ts
@@ -14,22 +14,25 @@ function addNullCamera() {
     newNull.name = 'Target Null'
     newNull.threeDLayer = true
 
-    const pos = (
-        newNull
-            .property('ADBE Transform Group')
-            .property('ADBE Position') as Property<ThreeDType>
-    ).value
+    const pos = newNull.position.value
 
     const targetCamera: CameraLayer = activeItem.layers.addCamera(
         'Targeting Camera',
         [pos[0], pos[1]]
     )
 
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     targetCamera
-        .property('ADBE Transfrom Group')
+        .property('ADBE Transform Group')
+        .property('ADBE Position')
+        // @ts-ignore
+        .setValue([pos[0], pos[1], targetCamera.position.value[2]])
+
+    targetCamera
+        .property('ADBE Transform Group')
         // @ts-ignore
         .property('ADBE Anchor Point').expression =
-        'thisComp.layer("Target Null")'
+        'thisComp.layer("Target Null").position'
 }
 
 addNullCamera()
